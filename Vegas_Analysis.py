f9e60cec67f5f1f81328289bfe6f0357f071ec86
wkdaniel3 <wkdaniel11@gmail.com>
2018-07-30 14:43:43 -0400

    Added exports to the python file

--- Vegas_Analysis.py
+++ Vegas_Analysis.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # Pull in yelp_business file
-dfYelp = pd.read_csv('yelp_business.csv', index_col = "business_id")
+dfYelp = pd.read_csv('yelp_business.csv', index_col = ("business_id"))
 
 dfVegas = dfYelp[dfYelp["city"] == "Las Vegas"]
 
@@ -33,8 +33,23 @@ del dfYelpHours
 # Pull in yelp_review file & filter down to just Vegas
 
 dfYelpReview = pd.read_csv('yelp_review.csv',
-                               index_col = "business_id")
+                               index_col = ("business_id","user_id"))
 
-dfVegasReview = dfYelpReview[dfYelpReview.index.isin(dfVegas.index)]
+dfVegasReview = dfYelpReview[dfYelpReview.index.get_level_values('business_id').isin(dfVegas.index)]
 
 del dfYelpReview
+
+# Pull in yelp_user file & filter down to just Vegas
+
+dfYelpUser = pd.read_csv('yelp_user.csv',
+                               index_col = "user_id")
+
+dfVegasUser = dfYelpUser[dfYelpUser.index.isin(dfVegasReview.index.get_level_values("user_id"))]
+
+del dfYelpUser
+
+dfVegas.to_csv("yelp_business_Vegas.csv")
+dfVegasAttributes.to_csv("yelp_business_attributes_Vegas.csv")
+dfVegasHours.to_csv("yelp_business_hours_Vegas.csv")
+dfVegasReview.to_csv("yelp_review_Vegas.csv")
+dfVegasUser.to_csv("yelp_user_Vegas.csv")
