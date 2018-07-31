acbe383a7db4e058b66ae6c53aa1b02ebcf839b9
Ruslan <asker4ik@gmail.com>
2018-07-31 11:40:18 -0400

    new version of a file

--- yelp_business_new_cleaned.py
+++ yelp_business_new_cleaned.py
@@ -1,10 +1,7 @@
-#TODO: Add comments
-#Filters the yelp_business csv to only restaurants, appends income data, and filters to non-missing income attribute
-
 import pandas as pd
 from collections import Counter
 
-yelp_business = pd.read_csv('yelp_business_new.csv', index_col = ("business_id"))
+yelp_business = pd.read_csv('yelp_business_new.csv')
 
 list_cat = [category for row in yelp_business['categories'] for category in row.split(';')]
 
@@ -19,18 +16,15 @@ def isRestaurant(row):
 
 yelp_business['isRestaurant'] = yelp_business['categories'].apply(isRestaurant)
 
-yelp_restaurants = yelp_business[(pd.notnull(yelp_business['pop_income'])) & (yelp_business['is_open'] != 0) & (yelp_business['isRestaurant']==1)]
+yelp_restaurants = yelp_business[yelp_business['isRestaurant']==1]
 
-yelp_restaurants = yelp_restaurants.drop(columns=['isRestaurant','neighborhood', 'address', 'Unnamed: 0'])
+del yelp_restaurants['isRestaurant']
 
-#yelp_restaurants.to_csv('yelp_restaurants.csv')
+yelp_restaurants = yelp_restaurants[pd.notnull(yelp_restaurants['pop_income'])]
 
-# Bring in yelp_business_attributes, and yelp_business hours to merge with yelp_restaurants
+yelp_restaurants = yelp_restaurants[yelp_restaurants['is_open'] != 0]
 
-dfYelpAttributes = pd.read_csv('yelp_business_attributes.csv',
-                               index_col = "business_id")
+yelp_restaurants = yelp_restaurants.drop(columns=['neighborhood', 'address', 'Unnamed: 0'])
 
-dfYelpHours = pd.read_csv('yelp_business_hours.csv',
-                               index_col = "business_id")
-
+yelp_restaurants.to_csv('yelp_restaurants.csv')
 
