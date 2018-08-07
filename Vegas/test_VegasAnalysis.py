43c15870208619a1c3748146625612d198ad9c45
wkdaniel3 <wkdaniel11@gmail.com>
2018-08-07 11:03:19 -0400

    update vegas testing

--- Vegas/test_VegasAnalysis.py
+++ Vegas/test_VegasAnalysis.py
@@ -1,6 +1,9 @@
 from Vegas_functions import sumCategoryData
 import unittest
 import pandas as pd
+import os
+
+os.chdir("..")
 
 # Pull in first row yelp_business        
 dfYelp = pd.read_csv('yelp_restaurants_CLEANED.csv', index_col = ("business_id"))
@@ -11,10 +14,12 @@ class test_sumCategoryData(unittest.TestCase):
     def test_categories_set_is_outputting_as_expected(self):
         dfTest = sumCategoryData(dfTopTwoRows)    
         testList = ['American (New)', 'American (Traditional)','Bars','Burgers','Nightlife','Salad','Sandwiches','Soup']
+        print(dfTest["name"].tolist())
         self.assertEqual(dfTest["name"].tolist(), testList, msg = "List did not contain expected categories!")
         
     def test_categories_count_is_outputting_as_expected(self):
         dfTest = sumCategoryData(dfTopTwoRows)
+        print(dfTest.loc[dfTest["name"]=="Sandwiches"]["distinct_count"].values[0])
         self.assertEqual(dfTest.loc[dfTest["name"]=="Sandwiches"]["distinct_count"].values[0], 2, msg = "Values are not adding up correctly")
 
 if __name__ == '__main__':
