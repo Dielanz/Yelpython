003f841dc3a2bf014ffad01f6bd0e8384abc2276
Ruslan <asker4ik@gmail.com>
2018-08-06 20:38:41 -0400

    header

--- Population_Income/Scatter_Hist_Pop_Income.py
+++ Population_Income/Scatter_Hist_Pop_Income.py
@@ -12,17 +12,22 @@ yelp_scatter = pd.read_csv('yelp_restaurants_CLEANED.csv', index_col = 'business
 
 #yelp_scatter = yelp_scatter.loc[:, ['stars', 'pop_income']]
 
+# creates two columns and splits the data from pop_income using a comma
 yelp_scatter[['population','income']] = yelp_scatter['pop_income'].str.split(',',expand=True)
 
+#removes the bracket from both population and income
 yelp_scatter['income'] = yelp_scatter['income'].map(lambda x: str(x)[:-1])
 yelp_scatter['population'] = yelp_scatter['population'].map(lambda x: str(x)[1:])
 
+#turns them to numbers
 yelp_scatter['population'] = pd.to_numeric(yelp_scatter['population'], errors='coerce')
 yelp_scatter['income'] = pd.to_numeric(yelp_scatter['income'], errors='coerce')
 
+# Apply log transformation to the data
 yelp_scatter['log_income'] = yelp_scatter['income'].apply(math.log)
 yelp_scatter['log_pop'] = yelp_scatter['population'].apply(math.log)
 
+#remove businesses that have less than 20 reviews
 yelp_scatter = yelp_scatter[yelp_scatter['review_count'] > 20]
 
 
@@ -31,11 +36,10 @@ yelp_scatter = yelp_scatter[yelp_scatter['review_count'] > 20]
 yelp_scatter.plot.scatter(x = 'log_pop', y = 'stars')
 yelp_scatter.plot.scatter(x = 'log_income', y = 'stars')
 
-states = list(set(yelp_scatter['state']))
-stars = list(set(yelp_scatter['stars']))
+states = list(set(yelp_scatter['state']))  #a list of unique states
+stars = list(set(yelp_scatter['stars']))  #a list of uniques star ratings
 
-
-
+# for each state calculate the average and plot the histogram for review counts
 for state in states:
     plt.figure()
     yelp_state = yelp_scatter[yelp_scatter['state']==state]
