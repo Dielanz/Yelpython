06650eb27bd372649a2fcc79cc2c75cb05e083d4
Ruslan <asker4ik@gmail.com>
2018-08-06 20:28:31 -0400

    header

--- Testing/AddIncomeTest.py
+++ Testing/AddIncomeTest.py
@@ -1,21 +1,20 @@
+# Testing the websraper
+
 import unittest
-from AddIncomeData_toBusinessCsv import *
+from Append_Pop_Income import *
 
 class TestIncome(unittest.TestCase):
-    
+
     ## test 1 for average city
     def test_income_1(self):
         print(pop_income_dict['Cuyahoga-Falls-Ohio'])
         self.assertEqual(pop_income_dict['Cuyahoga-Falls-Ohio'],(49210, 27862),'check population income file')
-    
-    
+
+
     ## test 2 for small city (fewer "," in population)
     def test_income_2(self):
         print(pop_income_dict['Phoenix-Arizona'])
-        self.assertEqual(pop_income_dict['Phoenix-Arizona'],(1615041, 26308),'check population income file')    
+        self.assertEqual(pop_income_dict['Phoenix-Arizona'],(1615041, 26308),'check population income file')
 
 if __name__ == '__main__':
-    unittest.main()  
-    
-    
-    
+    unittest.main()
